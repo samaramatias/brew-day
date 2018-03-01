79f047d037d84b7da07b21040c7274157be155a2
Bianca Lima <biancasllima@gmail.com>
2018-03-01 14:02:14 -0300

    Brew recipe tests

--- test/index.js
+++ test/index.js
@@ -48,14 +48,14 @@ var app = require('../app.js');
 
 test('Correct brew returned', function (assert) {
     request(app)
-      .get('/api/brew')
-      .set('access_token', 'TOKEN test')
-      .expect('Content-Type', 'asd')
-      .end(function (err, res) {
-        assert.same(res.status, 200);
-        assert.same(res.body, []);
-        assert.end();
-      });
+        .get('/api/brew')
+        .set('access_token', 'TOKEN test')
+        .expect('Content-Type', 'asd')
+        .end(function (err, res) {
+            assert.same(res.status, 200);
+            assert.same(res.body, []);
+            assert.end();
+        });
 }); 
 
 test('Create inventory correctly', function (assert) {
@@ -128,4 +128,83 @@ test('Correct recipe created', function (assert) {
         assert.notEqual(res.body._id, null);
         assert.end();
       });
-}); 
+});
+
+test('Create brew based on valid recipe', function (assert) {
+    var idInventory = "";
+    var recipeDb;
+    request(app)
+      .post('/api/inventory')
+      .set('access_token', 'TOKEN test')
+      .expect('Content-Type', 'asd')
+      .end(function (err, res) {
+        idInventory = res.body._id;
+        assert.same(res.status, 201);
+        assert.same(res.body.ingredients, []);
+      });
+
+    var inventoryPut = { 
+        _id: idInventory,
+        ingredients: 
+        [ { name: 'sugar', quantity: 20, unit: 'KG' },
+          {name: 'honey', quantity: 10, unit: 'L'},
+          {name: 'barley', quantity: 15, unit: 'KG'},
+          {name: 'special water', quantity: 50, unit: 'L'} ],
+        userId: user.user_id,
+        __v: 4
+    };  
+
+    request(app)
+        .get('/api/inventory')
+        .set('access_token', 'TOKEN test')
+        .expect('Content-Type', 'asd')
+        .end(function(err, res){
+            request(app)
+            .put('/api/inventory/'+res.body._id)
+            .set('access_token', 'TOKEN test')
+            .send({ 
+                _id: inventoryPut._id,
+                ingredients: 
+                [ { name: 'sugar', quantity: 2, unit: 'KG' },
+                {name: 'honey', quantity: 8, unit: 'L'},
+                {name: 'barley', quantity: 5, unit: 'KG'},
+                {name: 'special water', quantity: 20, unit: 'L'} ],
+                userId: user.user_id,
+                __v: 4
+            })
+            .end(function(error, response){
+                assert.same(response.status, 200);
+                assert.same(response.json);
+                assert.same(response.body.ingredients.length, 4);
+            });
+        });
+    request(app)
+        .post('/api/recipe')
+        .set('access_token', 'TOKEN test')
+        .expect('Content-Type', 'asd')
+        .send({ name: 'Test recipe',
+            directions: 'Try and try again',
+            ingredients: [ 
+                {name: 'honey', quantity: 0.25, unit: 'L'},
+                {name: 'barley', quantity: 0.5, unit: 'KG'},
+                {name: 'special water', quantity: 2, unit: 'L'} ],
+            equipment: { volume: 7, unit: 'L' } })
+        .end(function (err, responseRecipe) {
+            request(app)
+            .post('/api/brew')
+            .set('access_token', 'TOKEN test')
+            .expect('Content-Type', 'asd')
+            .send({ notes: "Test note",
+                    recipe: responseRecipe.body, 
+                    userId : user.user_id})
+            .end(function (error, responseBrew) {
+                // TODO drop DB to restart inventory quantities
+                assert.same(responseBrew.status, 400);
+                assert.equal(responseBrew.body._id);
+            });    
+        });
+
+        
+    
+});
+
